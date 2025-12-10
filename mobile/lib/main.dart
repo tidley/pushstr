6d9e312ecf5fcb8394f5690f89b39d234056b6e8
Tom Dwyer <tom.uwe@gmail.com>
2025-12-10 21:45:54 +0000

    bump

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1525,6 +1525,7 @@ class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
             ],
           ),
         ),
+        if (Platform.isAndroid) const SizedBox(height: 8),
       ],
     );
   }
